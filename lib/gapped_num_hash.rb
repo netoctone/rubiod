7e2e35abe594d9fb79e091530d7d84c10afe4f47
Eugen <netoctone@gmail.com>
2011-08-22 00:16:30 +0300

    refactored and extended GappedNumHash
    added method #taper

--- lib/gapped_num_hash.rb
+++ lib/gapped_num_hash.rb
@@ -1,62 +1,69 @@
 class GappedNumHash
   def initialize
-    @nums_gaps = [] # consists of: [key, val]
+    @pairs = [] # consists of [key, val]; key - [Range] x..y
   end
 
   def each &block
-    block ? @nums_gaps.each(&block) : @nums_gaps.each
+    block ? @pairs.each(&block) : @pairs.each
   end
 
   def last_index
-    last = @nums_gaps.last
+    last = @pairs.last
     last[0].last if last
   end
 
   # returns pair [key, val] or nil
   def at num
-    found = find_pair num
-    found[0] && [found[0], found[1]]
+    key, val = find_pair num
+    key && [key, val]
+  end
+
+  # num must belong to non-atom key, and it's 'width' must be > +by+
+  # returns pair [key, val]
+  def taper num, by=1
+    key, some_val, ind = find_pair num
+    raise ArgumentError if key.nil? || key.count <= by
+
+    @pairs[ind][0] = key.first+by..key.last
+    move_keys -by, ind
+    @pairs[ind]
   end
 
   # returns pair [key, val] or nil
-  def delete_at num
-    found = find_pair num
-    if found[0]
-      @nums_gaps.delete_at found[2]
-      [found[0], found[1]]
-    else
-      nil
+  def delete num
+    key, val, ind = find_pair num
+    if key
+      move_keys -1, ind
+      @pairs.delete_at ind
+      [key, val]
     end
   end
 
   # num must belong to atom key
   # returns pair [key, val]
   def insert_after num, val
-    f = find_pair num
-    raise ArgumentError unless f[0].atom?
+    key, some_val, ind = find_pair num
+    raise ArgumentError unless key && key.atom?
 
-    ins_ind = f[2] + 1
+    ins_ind = ind + 1
     pair = [num..num, val]
-    @nums_gaps[ins_ind...ins_ind] = [pair]
-    (ins_ind...@nums_gaps.size).each do |i|
-      cur_pair = @nums_gaps[i]
-      cur_pair[0] = cur_pair[0].move 1
-    end
+    @pairs[ins_ind...ins_ind] = [pair]
+    move_keys 1, ins_ind
     pair
   end
 
-  # num must belong non-atom key
+  # num must belong to non-atom key
   # val_hash - { :left => smth, :mid => smth, :right => smth }
   def insert_split num, val_hash
-    key, old_val, ind = find_pair(num)
-    raise ArgumentError if key.atom?
+    key, some_val, ind = find_pair(num)
+    raise ArgumentError if key.nil? || key.atom?
 
     new_pairs = []
     new_pairs << [key.first..num-1, val_hash[:left]] if num > key.first
     new_pairs << [num..num, val_hash[:mid]]
     new_pairs << [num+1..key.last, val_hash[:right]] if num < key.last
 
-    @nums_gaps[ind..ind] = new_pairs
+    @pairs[ind..ind] = new_pairs
   end
 
   # returns pair [key, val] or nil (when new key overlaps existing ones)
@@ -67,7 +74,7 @@ class GappedNumHash
       unless left[0] || right[0]
         if left[1] == right[1]
           pair = [key, val]
-          @nums_gaps[left[1]...left[1]] = [pair]
+          @pairs[left[1]...left[1]] = [pair]
           pair
         else
           nil
@@ -82,8 +89,8 @@ class GappedNumHash
 
   # returns val or nil
   def [] num
-    found = find_pair num
-    found[0] && found[1]
+    key, val = find_pair num
+    key && val
   end
 
   # may cause splitting
@@ -93,21 +100,28 @@ class GappedNumHash
     key = f[0]
     if key
       if key.atom?
-        @nums_gaps[f[2]] = val
+        @pairs[f[2]] = val
       else
         insert_split num, { :left => f[1], :mid => val, :right => f[1] }
         val
       end
-    else
-      @nums_gaps[f[1]...f[1]] = [[num..num, val]]
+    else # can make gaps between keys - quite unsafe
+      @pairs[f[1]...f[1]] = [[num..num, val]]
       val
     end
   end
 
   private
 
+  def move_keys direction, start_index=0
+    (start_index...@pairs.size).each do |i|
+      pair = @pairs[i]
+      pair[0] = pair[0].move direction
+    end
+  end
+
   def find_pair num
-    find_pair_iter num, 0, @nums_gaps.size-1
+    find_pair_iter num, 0, @pairs.size-1
   end
 
   # returns [key, val, index] or [nil, l_ind]
@@ -115,7 +129,7 @@ class GappedNumHash
     return [nil, l_ind] if l_ind > r_ind
 
     test_ind = (l_ind + r_ind)/2
-    test_pair = @nums_gaps[test_ind]
+    test_pair = @pairs[test_ind]
     test_key = test_pair[0]
     if test_key.include? num
       [test_pair, test_ind].flatten 1
